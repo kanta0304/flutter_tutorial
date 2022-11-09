0efb499e6752b81d0fd0121562997c27b15c9d0d
kanta0304 <v.persieunited20@gmail.com>
2022-11-09 15:05:20 +0900

    確認お願いします

--- lib/main.dart
+++ lib/main.dart
@@ -41,13 +41,119 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(
 
-          title: Text(widget.title),
+          title: Text(''),
           backgroundColor: Colors.white10,
         ),
-        body:Center(
-          child: Image.asset('images/oldtrafford.jpeg'),
+        body:Column(
+          children: [
+            Container(
+              height: 300,
+              width: 600,
+                padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                image: DecorationImage(
+                    image: AssetImage("images/oldtrafford.jpeg"),
+                    fit: BoxFit.cover
+                ),
+              ),
+                child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Icon(
+                        Icons.share,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 10,height: 20,),
+                      Icon(
+                        Icons.favorite_border,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width:10 ,height: 20,),
+                    ],
+                  ),
+                  Container(
+                    height: 110,
+                    width: 110,
+                  ),
+
+                  Text('オールド・トラフォード',style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.white,
+                  ),
+                ),
+                 SizedBox(height: 5,),
+                  Row(
+                  children: [
+                    Icon(
+                    Icons.location_on,
+                    color: Colors.white,
+                    ),
+                      Text('Manchester',style: TextStyle(
+                        color: Colors.white,
+                      ),),
+                  ]
+                  ),
+                  SizedBox(height: 5,),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        Icons.star_border,
+                        color: Colors.white,
+                      ),
+                      Text('4.0',style: TextStyle(
+                        color: Colors.white
+                      ),),
+                    ],
+                  )
+                ],
+              )
+            ),
+            SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+
+                  child: Icon(
+                    Icons.wifi,
+                  ),
+                ),
+                Icon(
+                  Icons.flatware,
+                ),
+                Icon(
+                  Icons.beach_access,
+                ),
+                Icon(
+                  Icons.more_horiz,
+                ),
+              ],
+            )
+          ],
         ),
+
     );
   }
 }
